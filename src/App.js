0e60dab99cc3ea4657c07711fc5150f8e0c60d90
Simomsl <mohammedelmarghani90@gmail.com>
2023-01-08 12:26:14 +0100

    qqqqq

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div>
-      <h1>version 2</h1>
+      <h1>NEW branch</h1>
       <h1 className="title">simo msl</h1>
       <h2>mohammed el marghani</h2>
     </div>
